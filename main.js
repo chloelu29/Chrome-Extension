ed49d95d9e569527bda36bcd9b75b60229462d82
scc135 <scc135@gmail.com>
2021-12-02 09:52:06 -0500

    added notes to readme and fixed the tab creation

--- main.js
+++ main.js
@@ -5,18 +5,7 @@
 //   console.log('Default background color set to %cgreen', `color: ${color}`);
 // });
 
-searchUrbanDict = function(word){
-    var query = word.selectionText;
-    chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
-    // let test = document.createElement('list');
-    // test.innerText='asdasda'
-    // document.getElementById('body').appendChild(test)
-  };
-  searchWikipedia = function(word){
-    var query = word.selectionText;
-    chrome.tabs.create({url: "https://en.wikipedia.org/w/index.php?search=" + query + "&title=Special%3ASearch&go=Go"});
-  };
-
+  chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: "UDMenu",
       title: "Search in UrbanDictionary",
@@ -29,8 +18,16 @@ searchUrbanDict = function(word){
       contexts:["selection"]
     });
 
+    chrome
+  });
+
   chrome.contextMenus.onClicked.addListener(function(selection) {
+    //selection is an object with menuItemId (the menu ids above), pageUrl (the tab's url that the selection was on), and selectionText (the selected text)
     console.log(selection);
+    
+    //if the menuItemIds match, create a new tab with the url
+    if (selection.menuItemId === 'UDMenu') chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + selection.selectionText});
+    if (selection.menuItemId === 'wikiMenu') chrome.tabs.create({url: "https://en.wikipedia.org/w/index.php?search=" + selection.selectionText + "&title=Special%3ASearch&go=Go"});
    })
 
 
